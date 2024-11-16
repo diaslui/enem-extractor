05936401a620e80771aa356974fdeac3c6b9f29d
scr6w <playerzone06@gmail.com>
2024-11-15 23:21:40 -0300

    feat: add question validation + validations

---
+++ src/settings.py
@@ -0,0 +1,9 @@
+"""
+2024 - Pedro Luis Dias
+https://github.com/luiisp/enem-extractor
+
+
+File intended for defining configurations about
+the test and how relationships with it should be made
+"""
+MAX_QUESTIONS: int = 90
---
+++ src/validations.py
@@ -0,0 +1,20 @@
+import re
+from settings import MAX_QUESTIONS
+
+
+def valid_question_number(s):
+    """
+    This tests whether it is a valid question.
+    It is a valid question if it has the term QUEST
+    and the numbering from 0 to MAX_QUESTIONS next to it.
+
+    Example:
+    valid_question_number("questão 01")  # True
+
+    :param s: str
+    :return: bool
+    
+    """
+    pattern = rf'\b(0?[0-9]|[1-8][0-9]|{MAX_QUESTIONS})\b'
+    match = re.search(pattern, s)
+    return match is not None
